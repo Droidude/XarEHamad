fc40c5975d9ee197eb5bfadf9644e7a7d63ae6a9
Zare Ahmer <softandweb007@gmail.com>
2015-04-11 13:44:25 +0500

    second commit

--- Dev/ZareAmmad/src/com/example/zareammad/MainActivity.java
+++ Dev/ZareAmmad/src/com/example/zareammad/MainActivity.java
@@ -2,17 +2,26 @@ package com.example.zareammad;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
 
+/**
+ * @author Tahla Khan
+ *
+ */
 public class MainActivity extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	
+		//AMMAD
+		
 	}
 
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
