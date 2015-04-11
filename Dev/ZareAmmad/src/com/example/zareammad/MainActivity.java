e213dca39c82b95444ffdf1a0eab93dc495dcdcb
Zare Ahmer <softandweb007@gmail.com>
2015-04-11 13:49:52 +0500

    yar bs kiya karun

--- Dev/ZareAmmad/src/com/example/zareammad/MainActivity.java
+++ Dev/ZareAmmad/src/com/example/zareammad/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends ActionBarActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	
-		//AMMAD
+		//AMMAD bhai the Greate...
 		
 	}
 
